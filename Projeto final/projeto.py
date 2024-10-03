e9e3d542de19af0fc762ea02e2029e3eec96457f
Fillypycleybson <fillypy.cley@gmail.com>
2024-10-03 08:47:20 -0300

    Update projeto.py

--- Projeto final/projeto.py
+++ Projeto final/projeto.py
@@ -1,21 +1,25 @@
 #Programadores: Fillypy Cleybson, Isaias Viana
 
-
 import webbrowser
 
 # Função para exibir os dados com o codigo do município e código do cargo 
 def lista_candidato(matriz, cod_municipio, cod_cargo, indice_procurado):
     resultados = []
     for item in matriz:
-        # Atribuindo as colunas certas para cod_municipio e cod_cargo
         if int(item[11]) == cod_municipio and int(item[13]) == cod_cargo:
-            # Retorna as informações com base nos índices procurados
              resultados.append([item[i] for i in indice_procurado])
-    
     if resultados:
-        for resultado in resultados:
-            print("\n".join(f"{i+1}: {valor}" for i, valor in enumerate(resultado)))
-            print("=-" * 20)  # Linha separadora entre os resultados
+       informs = ['Nome', 'Nome na Urna', 'Numero', 'Partido']
+       for resultado in resultados:
+           i = 0
+           for r in resultado:
+               if i < len(informs):
+                   print(f"{informs[i]}: {r}")
+               else:
+                   print(f"Item {i + 1}: {r}")
+               i += 1
+           print("=-" * 20)
+
     else:
         print('Município não encontrado, Cargo Inválido ou Candidato não encontrado')
 
@@ -24,20 +28,25 @@ def lista_candidato(matriz, cod_municipio, cod_cargo, indice_procurado):
 def candidato(matriz, cod_candidato, indice_procurado):
     resultados2 = []
     for dados_candidato in matriz:
-        if int(dados_candidato[16]) == cod_candidato:
+        if int(dados_candidato[15]) == cod_candidato:
             resultados2.append([dados_candidato[i] for i in indice_procurado])
-
     if resultados2:
+        informs = ['Nome', 'Nome na Urna', 'Numero', 'Partido']
         for resultado2 in resultados2:
-            print("\n".join(f"{i+1}: {valor}" for i, valor in enumerate(resultado2)))
-            print("=-" * 20)  # Linha separadora entre os resultados
+            i = 0 
+        for r in resultado2:
+            if i < len(informs):
+                print(f"{informs[i]}: {r}")
+            else:
+                print(f"Item {i + 1}: {r}") 
+            i += 1 
+        print("=-" * 20)
     else:
         print('Município não encontrado, Cargo Inválido ou Candidato não encontrado')
 
 
 # Informações que deseja extrair
-indice_procurado = [17, 18, 16, 25]
-# Abrindo o arquivo CSV
+indice_procurado = [17, 18, 16, 26]
 arq = open("consulta_cand_2024_PB.csv", "r")
 conteudo = arq.read()
 
@@ -58,7 +67,7 @@ while True:
     print('3- Abrir Página HTML ')
     print(' ')
     print('=-'*39)
-    opcao = int(input(''))
+    opcao = int(input('Digite: '))
     print('=-'*39)
     print('')
 
@@ -66,21 +75,22 @@ while True:
     if opcao == 1: 
         cod_municipio = int(input('Código do município: '))
         cod_cargo = int(input('Código do cargo: '))
+        print()
         lista_candidato(matriz, cod_municipio, cod_cargo, indice_procurado)
 
-    if opcao == 2:
+    elif opcao == 2:
         cod_candidato = int(input('Código do candidato: '))
+        print()
         candidato(matriz, cod_candidato, indice_procurado)
 
-    if opcao == 3:
-        caminho_arquivo = input("Digite o caminho completo do arquivo HTML: ")
-        webbrowser.open(f"file:///{caminho_arquivo}")
+    elif opcao == 3:
+        webbrowser.open('http://127.0.0.1:5500/pagina_info.html')
+        print('Pagina HTML foi aberta')
         
     else:
         print('Município não encontrado, Cargo Inválido ou Candidato não encontrado')
-        break
+    break
 
-#Fechar arquivo
 arq.close()
 
 
